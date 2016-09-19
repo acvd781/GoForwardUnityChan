f0a39847964df211945ef855d446c465214650fb
yuta <acvd781@gmail.com>
2016-09-19 19:28:17 +0900

    【課題提出】Lesson7 2Dゲームアプリを作ろう
    「課題：キューブの接触時に効果音をつけましょう」の提出。
    UnityChanタグを作成し、タグ判定によって効果音を鳴らすようにしました。
    スクリプトはCubeControllerに入れ込みました。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+    //キューブの移動速度
+    private float speed = -0.2f;
+    //消滅位置
+    private float deadLine = -10;
+    //効果音変数作成
+    private AudioSource syoutotuSE;
+
+	// Use this for initialization
+	void Start () {
+        //効果音入れ込み
+        syoutotuSE = GetComponent<AudioSource>();
+ 	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+        //画面外に出たら破棄する
+        if(transform.position.x < this.deadLine){
+            Destroy(gameObject);
+        }
+	
+	}
+    //衝突した対象がUnityChanタグ以外のときにSEを鳴らす
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag != "UnityChan") {
+            syoutotuSE.PlayOneShot(syoutotuSE.clip);
+        }
+    }
+}
